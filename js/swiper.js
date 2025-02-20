5522f1e393fa056a3a4827bd399bddf884a30da9
Maksym Drachenko <maks7d@gmail.com>
2025-02-21 02:11:54 +0500

    add button scroll

--- js/swiper.js
+++ js/swiper.js
@@ -95,3 +95,20 @@ const swiper = new Swiper('.swiper', {
     });
   });
 })();
+
+// Button to top
+document.addEventListener('DOMContentLoaded', function () {
+  const scrollButton = document.querySelector('.btn-scroll-up');
+  const servicesSection = document.getElementById('services');
+
+  window.addEventListener('scroll', function () {
+    const sectionTop = servicesSection.getBoundingClientRect().top;
+    const viewportHeight = window.innerHeight;
+
+    if (sectionTop < viewportHeight / 2) {
+      scrollButton.classList.add('show');
+    } else {
+      scrollButton.classList.remove('show');
+    }
+  });
+});
