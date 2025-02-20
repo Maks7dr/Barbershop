32bb4a9e125ecb38d73638dbf20cd0ec7554f60e
Maksym Drachenko <maks7d@gmail.com>
2025-02-20 13:59:39 +0500

    add smp

--- js/swiper.js
+++ js/swiper.js
@@ -6,6 +6,7 @@ const swiper = new Swiper('.swiper', {
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   },
 
   // Navigation arrows
@@ -17,6 +18,23 @@ const swiper = new Swiper('.swiper', {
   // And if we need scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
+  },
+  breakpoints: {
+    // Мобильные устройства
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10, // Отступ между слайдами
+    },
+    // Планшеты
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // Ноутбуки и выше
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
   },
 });
 
