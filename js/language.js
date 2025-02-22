ca1065f7219d7fbf2148b69a13eac738cc32ac09
Maksym Drachenko <maks7d@gmail.com>
2025-02-22 14:29:05 +0500

    add language to SMP section

---
+++ js/language.js
@@ -0,0 +1,90 @@
+const translations = {
+  ua: {
+    services: 'Послуги',
+    info: 'Про нас',
+    staff: 'Персонал',
+    gallery: 'Галерея',
+    contacts: 'Контакти',
+    work: 'Графік роботи Пн-Сб: 10:00 - 20:00',
+    heroTitle: 'Стильні стрижки',
+    heroText: 'Записуйтесь на стрижку вже зараз!',
+    heroTel: 'Записатися на стрижку',
+    servicesTitle: 'Наші послуги',
+    barber: 'Барбер',
+    barberText:
+      ' Ми створюємо не просто стрижки, а підкреслюємо індивідуальність кожного гостя. Тут панує атмосфера брутальноїестетики, затишку та безсервісу. Досвідчені барберипрацюють у кращих класичного чоловічого грумінгу,використовуючи пінструменти та косметику. Хочетеідеальну стрижку, боформлену бороду або просторозслабитися і під чавипити віскі? У <span class="text-golden" data-lang="barberGolden"  >Golden Arms</span>  на вас чекаєпрофесійний підхід, увага дта дружня атмосфера, вяку хочеться повертатися.',
+    barberGolden: 'Golden Arms',
+    makeup: 'Перманентний макіяж',
+    makeupText:
+      'Досконалість у кожній лінії – перманентний макіяж, який підкреслює вашу природну красу. Перманентний макіяж – це не просто процедура, а мистецтво, що дозволяє підкреслити переваги, створити ідеальну симетрію та позбавити вас від необхідності щоденного макіяжу. Завдяки сучасним технікам, якісним пігментам та професійному підходу результат виглядає максимально природно. Легка димка на бровах, виразний контур губ – все це робить образ більш яскравим та завершеним. Перманентний макіяж – це впевненість у собі щоранку, без зайвих зусиль. Будьте красивими завжди!',
+    pigmentation: 'Мікропігментація шкіри голови',
+    pigmentationText:
+      'Мікропігментація шкіри голови  – ефект густого волосся без хірургії. Мікропігментація шкіри голови <span class="text-golden">SMP</span> – це інноваційна процедура, яка створює візуальний ефект коротко стриженого волосся або збільшує його щільність. Це ідеальне рішення для тих, хто стикається з облисінням, рідким волоссям або шрамами на шкірі голови. Процедура виконується за допомогою спеціального обладнання та гіпоалергенних пігментів, які вводяться у верхні шари шкіри, імітуючи природний ріст волосся. Результат виглядає максимально природно та зберігається на довгі роки.',
+    servicesbtn: 'Подивитися ціни',
+    infoTitle:
+      'Ласкаво просимо до <span class="text-golden">Golden Arms</span> — місця, де стиль зустрічається з традицією.',
+    infoText:
+      ' Ми створюємо не просто стрижки, а підкреслюємо індивідуальність кожного гостя. Тут панує атмбрутальної естетики, затишку та бездоганного сервісу. Досвідчені бпрацюють у кращих традиціях класичного чоловічого грувикористовуючи преміальні інструменти та косметику. Хочете ідстрижку, бездоганно оформлену бороду або просто розслабипід час послуги випити віскі? У Golden Arms на васпрофесійний підхід, увага до деталей та дружня атмосфера,хочеться повертатися. <span class="text-golden">Goldenspan> — місце, де народжується ваш стиль.',
+    staffTitle: 'Наш персонал',
+    staffYaroslav: 'Ярослав',
+    staffYaroslavText:
+      'Понад 7 років у професії, тисячі задоволених клієнтів табездоганний смак – Ярік не просто стриже, а створює стиль, що підкреслює вашу індивідуальність. Класика чи сучасні тренди,  ідеально оформлена борода чи гоління небезпечною бритвою –кожен штрих його роботи пдо дрібниць. Він знає, як підібрати стрижку під форму структуру волосся та спосіб життя, щоб результат ідеально в будь-якій ситуації. У кріслі Яріка ви не просто отримаєте професійний догляд, але й зможете розслабитися в атмосфері затишку, чоловічого стилю та якісного сервісу. Ваш образ – його справа. Довіртесь професіоналу!',
+    staffTina: 'Тіна',
+    staffTinaText:
+      'Майстер із 7-річним стажем, який підкреслить вашу природну красу. З роками майстерності та увагою до деталей Тіна створює ідеальні лінії, ніжні відтінки та бездоганні форми, підкреслюючи унікальні риси кожного клієнта. Кожна робота — це поєднання техніки, індивідуального підходу та розуміння гармонії. Тіна використовує тільки професійне обладнання та якісні пігменти, щоб результат був природним, довговічним та комфортним. Важливо не просто нанести перманентний макіяж, а зберегти вашу природну привабливість, зробивши її виразнішою. Краса, яка залишається з вами надовго.',
+    staffZhenya: 'Женя',
+    staffZhenyaText:
+      'Опитний майстер з 5-річним стажем, який підкреслить вашу...',
+  },
+
+  pl: {
+    services: 'Usługi',
+    info: 'O nas',
+    staff: 'Personel',
+    gallery: 'Galeria',
+    contacts: 'Kontakt',
+    work: 'Godziny otwarcia: Pn-Sb: 10:00-20:00',
+    heroTitle: 'Stylowe fryzury',
+    heroText: 'Zapisz się na fryzurę już teraz!',
+    heroTel: 'Zapisz się na strzyżenie',
+    servicesTitle: 'Nasze usługi',
+    barber: 'Barber',
+    barberText:
+      'Tworzymy nie tylko fryzury, ale podkreślamy indywidualność każdego gościa. Panuje tu atmosfera brutalnej estetyki, przytulności i nienagannego serwisu. Doświadczeni barberzy pracują w najlepszych tradycjach klasycznego męskiego groomingu, używając premium narzędzi i kosmetyków. Chcesz idealną fryzurę, perfekcyjnie wystylizowaną brodę, czy po prostu zrelaksować się i wypić whisky podczas usługi? W <span class="text-golden" data-lang="barberGolden"  >Golden Arms</span> czeka na Ciebie profesjonalne podejście, dbałość o szczegóły i przyjazna atmosfera, do której chce się wracać.',
+    barberGolden: 'Golden Arms',
+    makeup: 'Makijaż permanentny',
+    makeupText:
+      'Doskonałość w każdej linii – makijaż permanentny, który podkreśla Twoje naturalne piękno. Makijaż permanentny to nie tylko zabieg, ale sztuka, która pozwala podkreślić atuty, stworzyć idealną symetrię i uwolnić Cię od konieczności codziennego makijażu. Dzięki nowoczesnym technikom, wysokiej jakości pigmentom i profesjonalnemu podejściu, rezultat wygląda maksymalnie naturalnie. Lekka mgiełka na brwiach, wyrazisty kontur ust – wszystko to sprawia, że obraz jest bardziej wyrazisty i kompletny. Makijaż permanentny to pewność siebie każdego ranka, bez zbędnego wysiłku. Bądź piękna zawsze!',
+    pigmentation: 'Mikropigmentacja skóry głowy',
+    pigmentationText:
+      'Mikropigmentacja skóry głowy <span class="text-golden">SMP</span> – efekt gęstych włosów bez chirurgii. Mikropigmentacja skóry głowy <span class="text-golden">(SMP)</span> to innowacyjny zabieg, który tworzy wizualny efekt krótko ostrzyżonych włosów lub zwiększa ich gęstość. To idealne rozwiązanie dla tych, którzy borykają się z łysieniem, rzadkimi włosami lub bliznami na skórze głowy. Zabieg wykonywany jest za pomocą specjalistycznego sprzętu i hipoalergicznych pigmentów, które są wprowadzane w górne warstwy skóry, imitując naturalny wzrost włosów. Rezultat wygląda maksymalnie naturalnie i utrzymuje się przez długie lata.',
+    servicesbtn: 'Zobacz ceny',
+    infoTitle:
+      'Witamy w <span class="text-golden">Golden Arms</span> — miejscu, gdzie styl spotyka się z tradycją.',
+    infoText:
+      'Tworzymy nie tylko fryzury, ale podkreślamy indywidualność każdego gościa. Panuje tu atmosfera brutalnej estetyki, przytulności i nienagannego serwisu. Doświadczeni barberzy pracują w najlepszych tradycjach klasycznego męskiego groomingu, używając premium narzędzi i kosmetyków. Chcesz idealną fryzurę, perfekcyjnie wystylizowaną brodę, czy po prostu zrelaksować się i wypić whisky podczas usługi? W Golden Arms czeka na Ciebie profesjonalne podejście, dbałość o szczegóły i przyjazna atmosfera, do której chce się wracać. <span class="text-golden">Golden Arms</span> — miejsce, gdzie rodzi się Twój styl.',
+    staffTitle: 'Nasz personel',
+    staffYaroslav: 'Jarosław',
+    staffYaroslavText:
+      'Ponad 7 lat w zawodzie, tysiące zadowolonych klientów i nienaganny smak – Yarik nie tylko strzyże, ale tworzy styl, który podkreśla Twoją indywidualność. Klasyka czy nowoczesne trendy, idealnie wystylizowana broda czy golenie brzytwą – każdy ruch jego pracy jest dopracowany do perfekcji. Wie, jak dobrać fryzurę do kształtu twarzy, struktury włosów i stylu życia, aby rezultat był idealny w każdej sytuacji. W fotelu Yarika nie tylko otrzymasz profesjonalną pielęgnację, ale także zrelaksujesz się w atmosferze przytulności, męskiego stylu i wysokiej jakości serwisu. Twój wygląd to jego sprawa. Zaufaj profesjonaliście!',
+    staffTina: 'Tina',
+    staffTinaText:
+      'Mistrz z 7-letnim doświadczeniem, który podkreśli Twoje naturalne piękno. Z latami mistrzostwa i dbałością o szczegóły Tina tworzy idealne linie, delikatne odcienie i nienaganne formy, podkreślając unikalne cechy każdego klienta. Każda praca to połączenie techniki, indywidualnego podejścia i zrozumienia harmonii. Tina używa tylko profesjonalnego sprzętu i wysokiej jakości pigmentów, aby rezultat był naturalny, trwały i komfortowy. Ważne jest nie tylko nałożenie makijażu permanentnego, ale także zachowanie Twojej naturalnej atrakcyjności, czyniąc ją bardziej wyrazistą. Piękno, które pozostaje z Tobą na długo.',
+    staffZhenya: 'Żenia',
+    staffZhenyaText:
+      'Doświadczony mistrz z 5-letnim stażem, który podkreśli Twoją...',
+  },
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const savedLang = localStorage.getItem('language') || 'ua';
+  changeLanguage(savedLang);
+});
+
+function changeLanguage(lang) {
+  if (!translations[lang]) return;
+  localStorage.setItem('language', lang);
+  document.querySelectorAll('[data-lang]').forEach(el => {
+    el.innerHTML = translations[lang][el.getAttribute('data-lang')];
+  });
+}
